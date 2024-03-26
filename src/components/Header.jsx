ddc5188753ef7c225ae411d22eb80d7b15f571e6
lamquangtruongnguyen <tnguyen10@my.hpu.edu>
2024-03-26 13:27:36 +0700

    initialize repo

---
+++ src/components/Header.jsx
@@ -0,0 +1,22 @@
+const Header = () => {
+  return (
+    <div>
+      <header>
+        <div>
+          <img src="../assets/logo.png" alt="" />
+        </div>
+        <ul>
+          <li>Solutions</li>
+          <li>Services</li>
+          <li>Careers</li>
+          <li>About Us</li>
+        </ul>
+        <div>
+          <button>Contact Us</button>
+        </div>
+      </header>
+    </div>
+  );
+};
+
+export default Header;
